3a8c3ded4c1d5aaa83431814d5748989e43763b7
wgorman <wgorman@pentaho.com>
2009-12-01 16:53:10 +0000

    added lgpl license headers to java files

--- src/org/pentaho/agilebi/pdi/spoon/AgileBISpoonPlugin.java
+++ src/org/pentaho/agilebi/pdi/spoon/AgileBISpoonPlugin.java
@@ -1,8 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2009 Pentaho Corporation..  All rights reserved.
+ */
 package org.pentaho.agilebi.pdi.spoon;
 
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
