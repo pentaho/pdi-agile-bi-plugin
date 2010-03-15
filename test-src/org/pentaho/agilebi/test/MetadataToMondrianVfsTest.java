4e0b62fadff5322f5564eb8ae750cb6b8ce524ac
wgorman <wgorman@pentaho.com>
2010-03-15 20:54:10 +0000

    fixed unit test by initing vfs

--- test-src/org/pentaho/agilebi/test/MetadataToMondrianVfsTest.java
+++ test-src/org/pentaho/agilebi/test/MetadataToMondrianVfsTest.java
@@ -7,13 +7,18 @@ import junit.framework.Assert;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.VFS;
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 import org.junit.Test;
+import org.pentaho.agilebi.vfs.MetadataToMondrianVfs;
 
 @SuppressWarnings("nls")
 public class MetadataToMondrianVfsTest {
   
   @Test
   public void testVfs() throws Exception {
+    
+    ((DefaultFileSystemManager)VFS.getManager()).addProvider("mtm", new MetadataToMondrianVfs());
+    
     FileSystemManager fsManager = VFS.getManager();
     FileObject fobj = fsManager.resolveFile("mtm:test-res/example_olap.xmi");
     StringBuilder buf = new StringBuilder(1000);
