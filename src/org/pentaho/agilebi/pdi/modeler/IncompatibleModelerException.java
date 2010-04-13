a0f501b38d1cd235fc9c321ef7f52ca57118222b
nbaker <nbaker@pentaho.com>
2010-04-13 20:47:33 +0000

    [AGILEBI-276] Adding a special key to saved Models. All other files will not be opened.

---
+++ src/org/pentaho/agilebi/pdi/modeler/IncompatibleModelerException.java
@@ -0,0 +1,9 @@
+package org.pentaho.agilebi.pdi.modeler;
+
+public class IncompatibleModelerException extends ModelerException{
+
+  public IncompatibleModelerException() {
+    super("Not a valid Model");   //$NON-NLS-1$
+  }
+
+}
