1e729b1a0356303ec8de2e18b6b4649b956c7a92
rmansoor <rmansoor@pentaho.com>
2011-07-19 18:43:04 +0000

    PDI-6024 - Updated the header

--- test-src/org/pentaho/agilebi/BrowserApp.java
+++ test-src/org/pentaho/agilebi/BrowserApp.java
@@ -1,19 +1,13 @@
-/*
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- * Foundation.
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- * or from the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * Copyright (c) 2009 Pentaho Corporation..  All rights reserved.
- */
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/ 
 package org.pentaho.agilebi;
 
 /*******************************************************************************
